49a031aecf7aa9077b4d5bb94620a6d7d4218474
Sidak <magicalsidak@gmail.com>
2024-08-08 19:11:14 -0400

    made my first ml model

---
+++ lesson_4.py
@@ -0,0 +1,67 @@
+#qestions
+#how does input function work
+#what is the correct amount of empohs and shuffle
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow.compat.v2.feature_column as fc
+
+import tensorflow as tf
+
+# Load dataset.
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
+                       'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+  vocabulary = dftrain[feature_name].unique()
+  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+    if shuffle:
+      ds = ds.shuffle(1000)
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    return ds
+  return input_function
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+linear_est.train(train_input_fn)
+
+#we made a ml model YAYAY
+
+result = linear_est.evaluate(eval_input_fn)
+
+clear_output()
+print(result['accuracy'])
+
+#73% accuracy lets go
+
+pred_dicts = list(linear_est.predict(eval_input_fn))
+probs = pd.Series([pred['probabilities'][1] for pred in pred_dicts])
+
+probs.plot(kind='hist', bins=20, title='predicted probabilities')
+
+passNum = 3
+print(dfeval.loc[passNum])
+print(y_eval.loc[passNum])
+print(result[passNum])
